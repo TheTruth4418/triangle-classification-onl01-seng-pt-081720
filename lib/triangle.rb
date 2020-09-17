fc0a4fa03186f1460a55e46e45850308628cb73e
William West <wwest466@gmail.com>
2020-09-17 16:39:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,8 @@ class Triangle
       :scalene
     elsif (@l2 = @l3) || (@l1 == @l2) || (@l1 == @l3)
       self.kind = :isosceles
-  else
-  raise TriangleError
+  elsif @l1 == 0 || @l2 == 0 || @l3 == 0
+    raise TriangleError
   end
   end
 end
